3a0a1745cfb20f4f5cb6427710496734fa6725c0
Andreas Andersen <etlos01@gmail.com>
2020-09-16 11:59:04 +0200

    Modified EMF + SQL scripts

--- mysqlscript(old).sql
+++ mysqlscript(old).sql
@@ -1,18 +1,46 @@
-INSERT INTO CAR (id, Year, Make, Model, Price)
+DROP DATABASE IF EXISTS `CA-1`;
+DROP DATABASE IF EXISTS `CA-1_Test`;
+CREATE SCHEMA `CA-1` ;
+CREATE SCHEMA `CA-1_Test` ;
+USE `CA-1`;
+
+
+CREATE TABLE `CA-1`.`car` (
+  `id` INT NOT NULL,
+  `Year` INT NOT NULL,
+  `Make` VARCHAR(45) NOT NULL,
+  `Model` VARCHAR(45) NOT NULL,
+  `Price` INT NOT NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `CA-1`.`member` (
+  `Studentid` VARCHAR(45) NOT NULL,
+  `Name` VARCHAR(45) NOT NULL,
+  `FavCandy` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`Studentid`));
+
+
+CREATE TABLE `CA-1`.`joke` (
+  `id` INT NOT NULL,
+  `Joke` VARCHAR(126) NOT NULL,
+  `Type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+  
+  INSERT INTO car (id, Year, Make, Model, Price)
 VALUES    (1, 1997, "Ford", "E350", 3000),
 		  (2, 1999, "Chevy", "Venture", 4900),
 		  (3, 2000, "Chevy", "Venture", 5000),
 		  (4, 1996, "Jeep", "Grand Cherokee", 4999),
 		  (5, 2005, "Volvo", "V70", 44799);
           
-INSERT INTO CLASSMEMBER (id, NAVN, FAVSLIK)
+INSERT INTO member (studentid, Name, FavCandy)
 VALUES ("na157", "Nicolas", "Salte Fisk"),
 	   ("aw116", "Alex", "Lakrids"),
        ("aa344", "Andreas", "Chokolade"),
        ("cf161", "Christoffer", "Vingummi");
        
        
-INSERT INTO JOKE (id, joke, type) 
+INSERT INTO joke (id, joke, type) 
 VALUES (1, "Hvad hedder verdens fattigste konge? Kong Kurs", "Plat"),
 	(2, "Hvorfor er fisk så grimme? De er vandskabt", "Plat"),
     (3, "Hvad sagde jesus da han satte sig ind i taxaen? Sømmet i bund!", "Farjokes"),
